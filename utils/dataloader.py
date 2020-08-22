ec25a2483cb774b7c73b84cce28d755bbb1764b5
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2020-08-22 21:47:58 +0430

    data loader created

---
+++ utils/dataloader.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+import h5py
+        
+def load_dataset(path_to_dataset, dataset_name):
+    if dataset_name == 'catvnotcat':
+        return __load_catvsnot(path_to_dataset)
+
+
+def __load_catvsnot(path_to_dataset):
+    train_dataset = h5py.File(os.path.join(path_to_dataset, 'train_catvnoncat.h5'), "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) 
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) 
+
+    test_dataset = h5py.File(os.path.join(path_to_dataset, 'test_catvnoncat.h5'), "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
