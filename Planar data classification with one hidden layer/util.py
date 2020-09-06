97aee5bbf01dc38bc9a9adc829fc3bf0475fb5c9
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2020-09-06 12:53:20 +0430

    minor change at nn utils

--- Planar data classification with one hidden layer/util.py
+++ Planar data classification with one hidden layer/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_decision_boundary(model, X, y):
+def plot_decision_boundary(model, X, y, model_typ='NONE'):
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
     y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
@@ -9,7 +9,10 @@ def plot_decision_boundary(model, X, y):
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     # Predict the function value for the whole grid
-    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    if model_typ=='NN':
+        Z = model.model(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        Z = model(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
